68a02887c5d615f985b72230c17765d706add217
Elena <60796137+elenazavala@users.noreply.github.com>
2022-05-31 12:50:48 -0500

    Update best-time-to-buy-and-sell-stock.py

--- python/best-time-to-buy-and-sell-stock.py
+++ python/best-time-to-buy-and-sell-stock.py
@@ -1,1 +1,14 @@
-
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_profit = 0
+        buy, sell = 0, 1 
+        
+        while sell < len(prices):
+            if prices[buy] < prices[sell]: # is it profitable?
+                profit = prices[sell] - prices[buy]
+                max_profit = max(profit,max_profit)
+            else:
+                buy = sell #we move our buy pointer wherever "sell" is, as it is lower and we want to buy at a lower price
+            sell += 1 #and we advance the sell pointer
+        
+        return max_profit
