274b93903e730c706d9be783351c4dbb6a5e6425
Elena <60796137+elenazavala@users.noreply.github.com>
2022-05-31 16:33:42 -0500

    Update subsets-ii.py

--- python/subsets-ii.py
+++ python/subsets-ii.py
@@ -1,1 +1,21 @@
-
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        path = []
+        
+        nums.sort()
+        
+        
+        self.dfs(nums, path, result)
+        
+        return result
+    
+    
+    def dfs(self, nums, path, result):
+        
+        result.append(path)
+        
+        
+        for i in range(len(nums)):
+            if i == 0 or nums[i-1] != nums[i]:
+                self.dfs(nums[i+1:], path + [nums[i]], result)
