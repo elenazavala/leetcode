9a87608bd75343bb9db888c197f912c656b369c9
Elena <60796137+elenazavala@users.noreply.github.com>
2022-06-08 15:20:03 -0500

    Update maximum-depth-of-binary-tree.js

--- javascript/maximum-depth-of-binary-tree.js
+++ javascript/maximum-depth-of-binary-tree.js
@@ -1,1 +1,28 @@
-
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = (root) => {
+    let maxDepth = 0;
+    let dfs = (node, depth) => 
+    {
+        if (!node){
+          return maxDepth;
+        }
+        if (depth > maxDepth){
+          maxDepth = depth;
+        }
+        dfs(node.right, depth + 1);
+        dfs(node.left, depth + 1);
+    }
+    dfs(root, 1);
+    return maxDepth;
+};
