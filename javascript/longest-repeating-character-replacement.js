9e5bd2c010f33176c081299ba45ac72d2ce25f2b
Elena <60796137+elenazavala@users.noreply.github.com>
2022-06-08 15:15:10 -0500

    Update longest-repeating-character-replacement.js

--- javascript/longest-repeating-character-replacement.js
+++ javascript/longest-repeating-character-replacement.js
@@ -1,1 +1,32 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+const characterReplacement = (s, k) => {
+  let left = 0;
+  let right = 0;
+  let max = 0; // max character count
+  const seen = {};
 
+  while (right < s.length) 
+  {
+    const char = s[right];
+    seen[char] = seen[char] ? seen[char] + 1 : 1;
+
+    if (seen[char] > max)
+    {
+      max = seen[char];
+    }
+    
+    if (right - left + 1 - max > k) 
+    {
+      seen[s[left]]--;
+      left++;
+    }
+
+    right++;
+  }
+
+  return right - left;
+};
