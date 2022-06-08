6383bbb20ee841993799d1b20ac21431c91647f0
Elena <60796137+elenazavala@users.noreply.github.com>
2022-06-08 15:04:39 -0500

    Update contains-duplicate.js

--- javascript/contains-duplicate.js
+++ javascript/contains-duplicate.js
@@ -1,1 +1,17 @@
 
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    let mappy = {};
+    
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] in mappy) {
+            return true;
+        } else {
+            mappy[nums[i]] = i;
+        }
+    }
+    return false;
+};
