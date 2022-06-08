ad3baca3b71222544fb98a900e4237a4f9397117
Elena <60796137+elenazavala@users.noreply.github.com>
2022-06-08 15:16:45 -0500

    Update longest-substring-without-repeating-characters.js

--- javascript/longest-substring-without-repeating-characters.js
+++ javascript/longest-substring-without-repeating-characters.js
@@ -1,1 +1,27 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (str) {
+  const mappy = {};
+  let start = 0;
+  let max = 0;
 
+  for (let i = 0; i < str.length; i++) {
+    let right_char = str[i];
+
+    if (!(right_char in mappy)) mappy[right_char] = 0;
+    mappy[right_char] += 1;
+
+    while (mappy[right_char] > 1) 
+    {
+      let left_char = str[start];
+      start += 1;
+
+      if (left_char in mappy) mappy[left_char] -= 1;
+      if (mappy[left_char] === 0) delete mappy[left_char];
+    }
+    max = Math.max(max, i - start + 1);
+  }
+  return max;
+};
