b2c37d7f9a7e0a99c2fa6ce5800e34892a00aef2
Elena <60796137+elenazavala@users.noreply.github.com>
2022-06-08 15:28:00 -0500

    Update valid-anagram.js

--- javascript/valid-anagram.js
+++ javascript/valid-anagram.js
@@ -1,1 +1,29 @@
-
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    let mappy = {};
+    
+    if (s.length !== t.length) {
+        return false;
+    }
+    
+    for (let i = 0; i < s.length; i++) {
+        if (mappy[s[i]]) {
+            mappy[s[i]]++;
+        } else {
+            mappy[s[i]] = 1;
+        }
+    }
+    
+    for (let i = 0;  i < t.length; i++) {
+        if (mappy[t[i]]) {
+            mappy[t[i]]--;
+        } else {
+            return false;
+        }
+    }
+    return true; 
+};
