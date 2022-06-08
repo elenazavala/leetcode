c36f944800ae09a16fab6294813a336b81a54e1e
Elena <60796137+elenazavala@users.noreply.github.com>
2022-06-08 15:25:21 -0500

    Update two-sum.js

--- javascript/two-sum.js
+++ javascript/two-sum.js
@@ -1,1 +1,15 @@
-
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let mappy = {};
+    for (let i = 0; i < nums.length; i++) {
+        if (target - nums[i] in mappy) {
+            return [mappy[target-nums[i]], i];
+        } else {
+            mappy[nums[i]] = i;
+        }
+    }
+};
