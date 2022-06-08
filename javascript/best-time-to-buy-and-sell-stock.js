c153fd5a4d64c8d09cabe71eec75fb47979de68a
Elena <60796137+elenazavala@users.noreply.github.com>
2022-06-08 15:08:15 -0500

    Update best-time-to-buy-and-sell-stock.js

--- javascript/best-time-to-buy-and-sell-stock.js
+++ javascript/best-time-to-buy-and-sell-stock.js
@@ -1,1 +1,18 @@
-
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let max_profit = 0;
+    let min = prices[0];
+    
+    for(let i = 1; i < prices.length; ++i) { 
+        if(min > prices[i]) {
+            min = prices[i];
+        } else if(prices[i] - min > max_profit) {
+            max_profit = prices[i] - min;
+        }
+    }
+    
+    return max_profit;
+};
