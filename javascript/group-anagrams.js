364d8fefbfc235dc49b7e9885b365c28b0487254
Elena <60796137+elenazavala@users.noreply.github.com>
2022-06-08 15:09:58 -0500

    Update group-anagrams.js

--- javascript/group-anagrams.js
+++ javascript/group-anagrams.js
@@ -1,1 +1,24 @@
 
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+const groupAnagrams = (strs) => {
+  const res = [];
+  const mappy = new Map();
+  for (let i = 0; i < strs.length; i++) {
+    const sorted = strs[i].split("").sort().join("");
+    
+    console.log(sorted);
+    if (mappy.has(sorted)) {
+      mappy.get(sorted).push(strs[i]); 
+    } else {
+      mappy.set(sorted, [strs[i]]); 
+    }
+  }
+
+  for (let [key, value] of mappy) {
+    res.push(value);
+  }
+  return res;
+};
