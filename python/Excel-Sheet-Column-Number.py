6d5093f498d8558aa9566dd774d291f4ebe8bcd5
Elena <60796137+elenazavala@users.noreply.github.com>
2022-08-30 16:25:54 -0700

    Update Excel-Sheet-Column-Number.py

--- python/Excel-Sheet-Column-Number.py
+++ python/Excel-Sheet-Column-Number.py
@@ -1,1 +1,13 @@
-
+class Solution:
+    def titleToNumber(self, s: str) -> int:
+        # alphabet = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+        if len(s) == 0:
+            return 0
+        
+  
+        solution = 0
+        for i in range(len(s)):
+            solution += ((ord(s[i])-64) * pow(26,len(s)-1-i))
+        
+        return solution
+        
