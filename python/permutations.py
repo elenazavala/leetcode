276c51aaaf443e93b1ac7a9db5be1838dd15af2b
Elena <60796137+elenazavala@users.noreply.github.com>
2022-08-30 17:37:42 -0700

    Update permutations.py

--- python/permutations.py
+++ python/permutations.py
@@ -1,1 +1,19 @@
-
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        permutations = []
+        
+        self.DFS(nums, permutations, result)
+        
+        return result
+    
+    
+    def DFS(self, nums, currentPermutation, result):
+        if len(nums) == 0:
+            result.append(currentPermutation)
+            
+        else:
+            for i in range(len(nums)):
+                newArray = nums[:i] + nums[i+1:]
+                newPermutation = currentPermutation + [nums[i]]
+                self.DFS(newArray, newPermutation, result)     
