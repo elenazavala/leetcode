8260cb3582583fea2c3b2f18ddc5259200670453
Elena <60796137+elenazavala@users.noreply.github.com>
2022-08-30 17:27:22 -0700

    Update valid-palindrome.py

--- python/valid-palindrome.py
+++ python/valid-palindrome.py
@@ -1,1 +1,22 @@
-
+class Solution(object):
+    def isPalindrome(self, s):
+    
+        # create a temp string skipping all non alphanumeric characters
+        temp = ""
+        for char in s:
+            if char.isalnum():
+                temp = temp + char 
+        temp = temp.lower()
+        print(temp)
+        #now we will have one huge word: temp
+        
+        l = 0
+        r = len(temp) - 1
+        
+        while l < r:
+            if temp[l] != temp[r]:
+                return False
+            l += 1
+            r -= 1
+            
+        return True
