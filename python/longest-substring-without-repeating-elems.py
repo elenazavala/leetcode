fb121a11a95368de123b824be53c9cd749748583
Elena <60796137+elenazavala@users.noreply.github.com>
2022-08-30 17:45:43 -0700

    Update longest-substring-without-repeating-elems.py

--- python/longest-substring-without-repeating-elems.py
+++ python/longest-substring-without-repeating-elems.py
@@ -1,1 +1,17 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+     
+        seen = set()
+        left = 0
+        res = 0
 
+        for right in range(len(s)):
+            while s[right] in seen: #move left pointer 1 after the repeater character
+                seen.remove(s[left]) #remove leftmost
+                left += 1 #slide window
+                
+            window_length = right - left + 1
+            res = max(window_length, res)
+            seen.add(s[right])
+
+        return res
