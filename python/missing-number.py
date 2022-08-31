707e251c622f86c74be6de3067e342ad2709f75a
Elena <60796137+elenazavala@users.noreply.github.com>
2022-08-30 17:39:52 -0700

    Update missing-number.py

--- python/missing-number.py
+++ python/missing-number.py
@@ -1,1 +1,15 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        map = {}
+        nums.append(nums[0])
+        
+        for i in range(len(nums)):
+            map[i] = i
+        
+        for i in range(len(nums)):
+            if nums[i] in map:
+                map.pop(nums[i])
+        
+        for key in map.keys():
+             return(key)
 
