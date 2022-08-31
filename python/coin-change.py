137c25d4a88bbdff28d9447c103eec27799ea29d
Elena <60796137+elenazavala@users.noreply.github.com>
2022-08-30 17:23:26 -0700

    Update coin-change.py

--- python/coin-change.py
+++ python/coin-change.py
@@ -1,1 +1,18 @@
-
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        # if index is less than denomination skip it
+        ways = [float("inf") for _ in range(amount + 1)]
+        
+        ways[0] = 0
+        
+       
+        for coin in coins:
+            for amount in range(1,len(ways)):
+                if coin <= amount:
+                    coinsNeeded = amount - coin                
+                    ways[amount] = min(ways[amount], ways[coinsNeeded] + 1)
+        
+        
+        
+        return ways[amount] if ways[amount] != float("inf") else -1
+        
