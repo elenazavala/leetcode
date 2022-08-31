b9fa2a4d23e14bc946d0a271a0db3f90e9741dd5
Elena <60796137+elenazavala@users.noreply.github.com>
2022-08-30 17:27:45 -0700

    Update valid-anagram.py

--- python/valid-anagram.py
+++ python/valid-anagram.py
@@ -1,1 +1,21 @@
-
+class Solution(object):
+    def isAnagram(self, s, t):
+        # for them to be anagrams they need to have
+        # the same # of letters, and the same character count
+         #if they are not the same length, return False immediately
+            
+        if len(s) != len(t):
+            return False
+        
+        sMap = {}
+        tMap = {}
+        
+        for i in range(len(s)):
+            charS, charT = s[i], t[i]
+            sMap[charS] = 1 + sMap.get(charS, 0)
+            tMap[charT] = 1 + tMap.get(charT, 0)
+        
+        if sMap == tMap:
+            return True
+        else:
+            return False
