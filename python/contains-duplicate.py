f083d7ea1d1063cdbc98fd0234aabadc112ae26c
Elena <60796137+elenazavala@users.noreply.github.com>
2022-08-30 17:24:10 -0700

    Update contains-duplicate.py

--- python/contains-duplicate.py
+++ python/contains-duplicate.py
@@ -1,1 +1,16 @@
-
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        
+        mappy = {}
+        
+        for num in nums:
+            if num in mappy:
+                return True
+            else:
+                mappy[num] = 1
+        
+        return False
