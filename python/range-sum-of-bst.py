e3538fdfdfd5d6c89fd5538045478085ffca902e
Elena <60796137+elenazavala@users.noreply.github.com>
2022-08-30 17:37:15 -0700

    Update range-sum-of-bst.py

--- python/range-sum-of-bst.py
+++ python/range-sum-of-bst.py
@@ -1,1 +1,28 @@
-
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rangeSumBST(self, root: TreeNode, low: int, high: int) -> int:
+        self.currentSum = 0
+        self.dfs(root,low,high)
+        
+        return self.currentSum
+        
+        
+    def dfs(self, root, low, high):  
+        
+        if root is None: 
+            return # return self.currentSum
+        if root.val >= low and root.val <= high:  
+            self.currentSum += root.val  
+            
+        if root.val > low:
+            self.dfs(root.left, low, high) 
+        if root.val < high:
+             self.dfs(root.right, low, high)           
+            
+                
+        # return self.currentSum 
