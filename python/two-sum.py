fd468844d5d213afdc5f7703a7dda476ec251afe
Elena <60796137+elenazavala@users.noreply.github.com>
2022-08-30 17:28:17 -0700

    Update two-sum.py

--- python/two-sum.py
+++ python/two-sum.py
@@ -1,1 +1,11 @@
-
+class Solution(object):
+    def twoSum(self, nums, target):
+        
+        mappy = {}
+        for i in range(len(nums)):
+            diff = target - nums[i] #ver si hay este numero "diff" en el mapa. Si hay, encontramos el two sum
+            if diff in mappy:
+                return mappy[diff], i #retornar index del current number y de diff
+            else:
+                mappy[nums[i]] = i  # aca mappeamos el index con el numero en el q estamos
+        
