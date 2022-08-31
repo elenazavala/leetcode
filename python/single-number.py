5cdb2e0320b510caa7bd84f036321cf3701e5cd7
Elena <60796137+elenazavala@users.noreply.github.com>
2022-08-30 17:33:16 -0700

    Update single-number.py

--- python/single-number.py
+++ python/single-number.py
@@ -1,1 +1,9 @@
-
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        res = 0
+        
+        for number in nums:
+            res = res ^ number
+        
+        
+        return res
