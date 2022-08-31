76e90555578ac659cfae6f5a632e389382b32bc9
Elena <60796137+elenazavala@users.noreply.github.com>
2022-08-30 17:38:03 -0700

    Update permutations-ii.py

--- python/permutations-ii.py
+++ python/permutations-ii.py
@@ -1,1 +1,18 @@
-
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        currentPermutation = []
+        
+        self.DFS(nums, currentPermutation, result)        
+        return result
+    
+    def DFS(self, nums, currentPermutation, result):
+        if len(nums) == 0:
+            if currentPermutation not in result:
+                result.append(currentPermutation)
+                
+        else:
+            for i in range(len(nums)):
+                newArray = nums[:i] + nums[i+1:]
+                newPermutation = currentPermutation + [nums[i]]
+                self.DFS(newArray, newPermutation, result)
