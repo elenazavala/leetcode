8d9218b77cc953b029e52abf391436a1bb325f90
Elena <60796137+elenazavala@users.noreply.github.com>
2022-08-30 17:26:10 -0700

    Update defanging-an-ip-address.py

--- python/defanging-an-ip-address.py
+++ python/defanging-an-ip-address.py
@@ -1,1 +1,12 @@
-
+class Solution:
+    def defangIPaddr(self, address: str) -> str:
+        defanged = ""
+        
+        for char in address:
+            if char != '.':
+                defanged += char
+                continue
+            
+            defanged +='[.]'
+                
+        return defanged
