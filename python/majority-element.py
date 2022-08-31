a6f224761643ff624efd473c6cb084b50a9ef364
Elena <60796137+elenazavala@users.noreply.github.com>
2022-08-30 17:45:01 -0700

    Update majority-element.py

--- python/majority-element.py
+++ python/majority-element.py
@@ -1,1 +1,22 @@
-
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        table = {}
+        
+        for key in nums:
+            if key in table:
+                table[key] += 1               
+            else:
+                table[key] = 1
+            
+            if table[key] > len(nums)/2:
+                    return key
+            
+        # return None
+            
+            
+        # for key in table:
+        #     if table[key] > len(nums)/2:
+        #         return key
+        
+             
+        
